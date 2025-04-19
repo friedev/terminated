4eecbe9c42993dc26768b59db21b655100bed785
Aaron Friesen <aaron@frie.dev>
2025-04-19 15:46:00 -0500

    Replace TileMap with TileMapLayers

--- scenes/main.gd
+++ scenes/main.gd
@@ -9,34 +9,34 @@ class Wave:
 		self.enemies = enemies
 
 var waves := [
-	Wave.new(0.1, [4,  0, 0, 0, 0]),
-	Wave.new(6,   [8,  0, 0, 0, 0]),
-	Wave.new(8,   [8,  0, 0, 0, 0]),
-	Wave.new(10,  [8,  0, 0, 0, 0]),
-	Wave.new(16,  [0,  2, 0, 0, 0]),
-	Wave.new(22,  [16, 0, 0, 0, 0]),
-	Wave.new(24,  [16, 0, 0, 0, 0]),
-	Wave.new(30,  [0,  4, 0, 0, 0]),
-	Wave.new(36,  [16, 0, 0, 0, 0]),
-	Wave.new(38,  [16, 2, 0, 0, 2]),
-	Wave.new(40,  [0,  2, 0, 0, 2]),
-	Wave.new(44,  [16, 0, 0, 0, 4]),
-	Wave.new(48,  [16, 0, 0, 0, 4]),
-	Wave.new(52,  [4,  0, 1, 0, 0]),
-	Wave.new(56,  [4,  0, 1, 0, 0]),
-	Wave.new(62,  [0,  2, 2, 0, 0]),
-	Wave.new(66,  [16, 2, 0, 0, 0]),
-	Wave.new(68,  [0,  0, 2, 0, 2]),
-	Wave.new(76,  [0,  0, 0, 4, 0]),
-	Wave.new(80,  [0,  0, 0, 4, 0]),
-	Wave.new(90,  [16, 0, 0, 0, 0]),
-	Wave.new(92,  [16, 0, 0, 0, 0]),
-	Wave.new(94,  [16, 0, 0, 0, 0]),
-	Wave.new(96,  [16, 0, 0, 0, 0]),
-	Wave.new(110, [0,  8, 0, 0, 0]),
-	Wave.new(116, [0,  0, 3, 0, 0]),
-	Wave.new(126, [0,  0, 0, 8, 0]),
-	Wave.new(128, [0,  0, 0, 0, 4]),
+	Wave.new(0.1, [4, 0, 0, 0, 0]),
+	Wave.new(6, [8, 0, 0, 0, 0]),
+	Wave.new(8, [8, 0, 0, 0, 0]),
+	Wave.new(10, [8, 0, 0, 0, 0]),
+	Wave.new(16, [0, 2, 0, 0, 0]),
+	Wave.new(22, [16, 0, 0, 0, 0]),
+	Wave.new(24, [16, 0, 0, 0, 0]),
+	Wave.new(30, [0, 4, 0, 0, 0]),
+	Wave.new(36, [16, 0, 0, 0, 0]),
+	Wave.new(38, [16, 2, 0, 0, 2]),
+	Wave.new(40, [0, 2, 0, 0, 2]),
+	Wave.new(44, [16, 0, 0, 0, 4]),
+	Wave.new(48, [16, 0, 0, 0, 4]),
+	Wave.new(52, [4, 0, 1, 0, 0]),
+	Wave.new(56, [4, 0, 1, 0, 0]),
+	Wave.new(62, [0, 2, 2, 0, 0]),
+	Wave.new(66, [16, 2, 0, 0, 0]),
+	Wave.new(68, [0, 0, 2, 0, 2]),
+	Wave.new(76, [0, 0, 0, 4, 0]),
+	Wave.new(80, [0, 0, 0, 4, 0]),
+	Wave.new(90, [16, 0, 0, 0, 0]),
+	Wave.new(92, [16, 0, 0, 0, 0]),
+	Wave.new(94, [16, 0, 0, 0, 0]),
+	Wave.new(96, [16, 0, 0, 0, 0]),
+	Wave.new(110, [0, 8, 0, 0, 0]),
+	Wave.new(116, [0, 0, 3, 0, 0]),
+	Wave.new(126, [0, 0, 0, 8, 0]),
+	Wave.new(128, [0, 0, 0, 0, 4]),
 	Wave.new(140, [16, 2, 1, 1, 2]),
 ]
 
@@ -71,11 +71,17 @@ var flock_center: Vector2
 var flock_heading: Vector2
 
 @onready var player: Player = %Player
-@onready var tile_map: TileMap = %TileMap
+@onready var wall_tile_map: TileMapLayer = $WallTileMap
+@onready var floor_tile_map: TileMapLayer = $FloorTileMap
 @onready var spawn_timer: Timer = %SpawnTimer
 @onready var main_menu: Control = %MainMenu
 
 func _ready() -> void:
+	assert(
+		self.wall_tile_map.tile_set.tile_size == self.floor_tile_map.tile_set.tile_size,
+		"Expected wall and floor tile sizes to match"
+	)
+
 	self.player.visible = false
 	self.player.set_process(false)
 	self.player.set_physics_process(false)
@@ -86,9 +92,9 @@ func setup_tilemap() -> void:
 	for x in range(0, self.map_size.x + 2):
 		for y in range(0, self.map_size.y + 2):
 			var v := Vector2i(x, y)
-			self.tile_map.set_cell(1, v, 0, self.FLOOR_COORDS)
+			self.floor_tile_map.set_cell(v, 0, self.FLOOR_COORDS)
 			if x == 0 or x == self.map_size.x + 1 or y == 0 or y == self.map_size.y + 1:
-				self.tile_map.set_cell(0, v, 0, self.BORDER_COORDS)
+				self.wall_tile_map.set_cell(v, 0, self.BORDER_COORDS)
 
 
 func setup() -> void:
@@ -107,7 +113,7 @@ func setup() -> void:
 	self.final_wave_delay = final_wave_initial_delay
 
 	self.player.global_position = Vector2(
-		self.tile_map.tile_set.tile_size
+		self.floor_tile_map.tile_set.tile_size
 		* (self.map_size + Vector2i.ONE * 2)
 	) * 0.5
 	self.player.setup()
@@ -134,14 +140,14 @@ func spawn_enemy(enemy_scene: PackedScene) -> void:
 	var spawn_position
 	# TODO in theory, we shouldn't need to subtract one from map_size.x/y here
 	if randi() % 2 == 0:
-		spawn_position = self.tile_map.map_to_local(
+		spawn_position = self.floor_tile_map.map_to_local(
 			Vector2i(
 				randi() % (self.map_size.x - 1),
 				0 if randi() % 2 == 0 else self.map_size.y - 1
 			) + Vector2i.ONE
 		)
 	else:
-		spawn_position = self.tile_map.map_to_local(
+		spawn_position = self.floor_tile_map.map_to_local(
 			Vector2i(
 				0 if randi() % 2 == 0 else self.map_size.x - 1,
 				randi() % self.map_size.y - 1
