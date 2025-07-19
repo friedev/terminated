a7a983ce8794c5e1d0a03fcb0c2ae696d8088995
Aaron Friesen <aaron@frie.dev>
2025-07-19 17:28:56 -0500

    Add options to show timer/FPS
    - Remove unused input actions

--- scenes/ui/fps_label.gd
+++ scenes/ui/fps_label.gd
@@ -1,9 +1,5 @@
 extends Label
 
 func _process(_delta: float) -> void:
+	self.visible = Options.options["show_fps"]
 	text = "FPS: %d" % Engine.get_frames_per_second()
-
-
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("fps"):
-		visible = not visible
--- scenes/ui/timer_label.gd
+++ scenes/ui/timer_label.gd
@@ -12,12 +12,10 @@ func update_timer() -> void:
 func _process(_delta: float) -> void:
 	# Stop timer after player dies
 	if Player.instance.alive:
+		visible = Options.options["show_timer"]
 		update_timer()
-
-
-func _on_main_game_started() -> void:
-	show()
 
 
 func _on_player_died() -> void:
 	update_timer()
+	show()
