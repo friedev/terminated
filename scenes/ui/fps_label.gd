f6897845cad1b12d1bd8074ffa8651008304b165
Aaron Friesen <aaron@frie.dev>
2026-05-23 22:09:20 -0500

    Remove unidiomatic use of self keyword

--- scenes/ui/fps_label.gd
+++ scenes/ui/fps_label.gd
@@ -1,5 +1,5 @@
 extends Label
 
 func _process(_delta: float) -> void:
-	self.visible = Options.options["show_fps"]
+	visible = Options.options["show_fps"]
 	text = "FPS: %d" % Engine.get_frames_per_second()
